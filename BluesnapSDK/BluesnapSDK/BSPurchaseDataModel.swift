8bc913c4029ecf305369afaaa734fc53151b5a5c
Roy Biber <roy.biber@bluesnap.com>
2018-11-05 11:48:56 +0200

    added sdkRequestShopperRequirments

--- BluesnapSDK/BluesnapSDK/BSPurchaseDataModel.swift
+++ BluesnapSDK/BluesnapSDK/BSPurchaseDataModel.swift
@@ -11,7 +11,7 @@ import Foundation
 /**
  Available payment types
  */
-public enum BSPaymentType : String {
+public enum BSPaymentType: String {
     case CreditCard = "CC"
     case ApplePay = "APPLE_PAY"
     case PayPal = "PAYPAL"
@@ -22,7 +22,8 @@ public enum BSPaymentType : String {
  Base class for the different payments; for now only BSCreditCardInfo inherits from this.
  */
 @objc public class BSPaymentInfo: NSObject {
-    let paymentType : BSPaymentType!
+    let paymentType: BSPaymentType!
+
     public init(paymentType: BSPaymentType!) {
         self.paymentType = paymentType
     }
@@ -31,31 +32,31 @@ public enum BSPaymentType : String {
 /**
  Base class for payment request; this will be the result of the payment flow (one of the inherited classes: BSCcSdkResult/BSApplePaySdkResult/BSPayPalSdkResult)
  */
-@objc public class BSBaseSdkResult : NSObject {
-    
+@objc public class BSBaseSdkResult: NSObject {
+
     var fraudSessionId: String?
     var priceDetails: BSPriceDetails!
-    
+
     internal init(sdkRequest: BSSdkRequest) {
         super.init()
         self.priceDetails = sdkRequest.priceDetails.copy() as! BSPriceDetails
         self.fraudSessionId = BlueSnapSDK.fraudSessionId
     }
-    
+
     public func getFraudSessionId() -> String? {
         return fraudSessionId;
     }
-        
+
     // MARK: getters and setters
-    
+
     public func getAmount() -> Double! {
         return priceDetails.amount.doubleValue
     }
-    
+
     public func getTaxAmount() -> Double! {
         return priceDetails.taxAmount.doubleValue
     }
-    
+
     public func getCurrency() -> String! {
         return priceDetails.currency
     }
@@ -65,11 +66,11 @@ public enum BSPaymentType : String {
 /**
  price details: amount, tax and currency
  */
-@objc public class BSPriceDetails : NSObject, NSCopying {
+@objc public class BSPriceDetails: NSObject, NSCopying {
 
     public var amount: NSNumber! = 0.0
     public var taxAmount: NSNumber! = 0.0
-    public var currency : String! = "USD"
+    public var currency: String! = "USD"
 
     @objc public func setDetailsWithAmount(amount: NSNumber!, taxAmount: NSNumber!, currency: NSString?/*, baseCurrency: NSString?*/) {
         self.amount = amount
@@ -83,14 +84,14 @@ public enum BSPaymentType : String {
         self.taxAmount = NSNumber.init(value: taxAmount)
         self.currency = currency ?? "USD"
     }
-    
+
     public func copy(with zone: NSZone? = nil) -> Any {
         let copy = BSPriceDetails(amount: amount.doubleValue, taxAmount: taxAmount.doubleValue, currency: currency)
         return copy
     }
-    
+
     public func changeCurrencyAndConvertAmounts(newCurrency: BSCurrency!) {
-        
+
         if let currencies = BSApiManager.bsCurrencies {
             let originalRate = currencies.getCurrencyByCode(code: self.currency)?.getRate() ?? 1.0
             self.currency = newCurrency.code
@@ -111,36 +112,55 @@ public enum BSPaymentType : String {
     - (optional) Shopper details
     - (optional) function for updating tax amount based on shipping country/state. Only called when 'withShipping
  */
-@objc public class BSSdkRequest : NSObject {
+@objc public class BSSdkRequest: BSSdkRequestShopperRequirements {
+    public var priceDetails: BSPriceDetails! = BSPriceDetails(amount: 0, taxAmount: 0, currency: nil)
+    public var updateTaxFunc: ((_ shippingCountry: String, _ shippingState: String?, _ priceDetails: BSPriceDetails) -> Void)?
+
+    public init(
+            withEmail: Bool,
+            withShipping: Bool,
+            fullBilling: Bool,
+            priceDetails: BSPriceDetails!,
+            billingDetails: BSBillingAddressDetails?,
+            shippingDetails: BSShippingAddressDetails?,
+            purchaseFunc: @escaping (BSBaseSdkResult!) -> Void,
+            updateTaxFunc: ((_ shippingCountry: String, _ shippingState: String?, _ priceDetails: BSPriceDetails) -> Void)?) {
+
+        super.init(withEmail: withEmail,
+                withShipping: withShipping,
+                fullBilling: fullBilling,
+                billingDetails: billingDetails,
+                shippingDetails: shippingDetails,
+                purchaseFunc: purchaseFunc)
+        self.priceDetails = priceDetails
+        self.updateTaxFunc = updateTaxFunc
+    }
+}
+
+@objc public class BSSdkRequestShopperRequirements: NSObject {
 
     public var withEmail: Bool = true
     public var withShipping: Bool = false
     public var fullBilling: Bool = false
-    public var priceDetails: BSPriceDetails! = BSPriceDetails(amount: 0, taxAmount: 0, currency: nil)
-    
+
     public var billingDetails: BSBillingAddressDetails?
     public var shippingDetails: BSShippingAddressDetails?
 
     public var purchaseFunc: (BSBaseSdkResult!) -> Void
-    public var updateTaxFunc: ((_ shippingCountry: String, _ shippingState: String?, _ priceDetails: BSPriceDetails) -> Void)?
-    
+
     public init(
-        withEmail: Bool,
-        withShipping: Bool,
-        fullBilling: Bool,
-        priceDetails: BSPriceDetails!,
-        billingDetails: BSBillingAddressDetails?,
-        shippingDetails: BSShippingAddressDetails?,
-        purchaseFunc: @escaping (BSBaseSdkResult!) -> Void,
-        updateTaxFunc: ((_ shippingCountry: String, _ shippingState: String?, _ priceDetails: BSPriceDetails) -> Void)?) {
-        
+            withEmail: Bool,
+            withShipping: Bool,
+            fullBilling: Bool,
+            billingDetails: BSBillingAddressDetails?,
+            shippingDetails: BSShippingAddressDetails?,
+            purchaseFunc: @escaping (BSBaseSdkResult!) -> Void) {
+
         self.withEmail = withEmail
         self.withShipping = withShipping
         self.fullBilling = fullBilling
-        self.priceDetails = priceDetails
         self.billingDetails = billingDetails
         self.shippingDetails = shippingDetails
         self.purchaseFunc = purchaseFunc
-        self.updateTaxFunc = updateTaxFunc
     }
 }
