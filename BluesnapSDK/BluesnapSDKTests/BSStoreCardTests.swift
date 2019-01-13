4d4c8cf9ad2190aa1a3de4e06ed31a5dcddd67b3
Roy Biber <roy.biber@bluesnap.com>
2019-01-13 15:50:13 +0200

    added integration testing for update shopper

--- BluesnapSDK/BluesnapSDKTests/BSStoreCardTests.swift
+++ BluesnapSDK/BluesnapSDKTests/BSStoreCardTests.swift
@@ -33,11 +33,47 @@ class BSStoreCardTests: XCTestCase {
                 purchaseFunc: { _ in }
         )
 
+        XCTAssertTrue(!sdkRequestShopperRequirements.hideStoreCardSwitch)
+
         sdkRequestShopperRequirements.hideStoreCardSwitch = false
         XCTAssertTrue(!sdkRequestShopperRequirements.hideStoreCardSwitch)
 
         sdkRequestShopperRequirements.hideStoreCardSwitch = true
         XCTAssertTrue(!sdkRequestShopperRequirements.hideStoreCardSwitch)
+
+        print("----------------------------------------------------")
+
+        var sdkResultShopperRequirementsApplePay = BSApplePaySdkResult(sdkRequestBase: sdkRequestShopperRequirements)
+        XCTAssertNil(sdkResultShopperRequirementsApplePay.storeCard)
+
+        var sdkResultShopperRequirementsPayPal = BSPayPalSdkResult(sdkRequestBase: sdkRequestShopperRequirements)
+        XCTAssertNil(sdkResultShopperRequirementsPayPal.storeCard)
+
+        var sdkResultShopperRequirementsCC = BSCcSdkResult(sdkRequestBase: sdkRequestShopperRequirements)
+        XCTAssertTrue(sdkResultShopperRequirementsCC.storeCard)
+
+        sdkResultShopperRequirementsCC.storeCard = false
+        XCTAssertTrue(sdkResultShopperRequirementsCC.storeCard)
+
+        sdkResultShopperRequirementsCC.storeCard = true
+        XCTAssertTrue(sdkResultShopperRequirementsCC.storeCard)
+
+        sdkResultShopperRequirementsCC.storeCard = false
+        XCTAssertTrue(sdkResultShopperRequirementsCC.storeCard)
+
+        var sdkResultExistingShopperRequirementsCC: BSExistingCcSdkResult = BSExistingCcSdkResult(sdkRequestBase: sdkRequestShopperRequirements, shopper: BSShopper(), existingCcDetails: BSCreditCardInfo(creditCard: BSCreditCard(), billingDetails: nil))
+        XCTAssertTrue(sdkResultExistingShopperRequirementsCC.storeCard)
+
+        sdkResultExistingShopperRequirementsCC.storeCard = false
+        XCTAssertTrue(sdkResultExistingShopperRequirementsCC.storeCard)
+
+        sdkResultExistingShopperRequirementsCC.storeCard = true
+        XCTAssertTrue(sdkResultExistingShopperRequirementsCC.storeCard)
+
+        sdkResultExistingShopperRequirementsCC.storeCard = false
+        XCTAssertTrue(sdkResultExistingShopperRequirementsCC.storeCard)
+
+        print("----------------------------------------------------")
 
         var sdkRequest = BSSdkRequest(withEmail: false,
                 withShipping: false,
@@ -56,5 +92,37 @@ class BSStoreCardTests: XCTestCase {
 
         sdkRequest.hideStoreCardSwitch = false
         XCTAssertFalse(sdkRequest.hideStoreCardSwitch)
+
+        print("----------------------------------------------------")
+
+        var sdkResultApplePay = BSApplePaySdkResult(sdkRequestBase: sdkRequest)
+        XCTAssertNil(sdkResultApplePay.storeCard)
+
+        var sdkResultPayPal = BSPayPalSdkResult(sdkRequestBase: sdkRequest)
+        XCTAssertNil(sdkResultPayPal.storeCard)
+
+        var sdkResultCC = BSCcSdkResult(sdkRequestBase: sdkRequest)
+        XCTAssertFalse(sdkResultCC.storeCard)
+
+        sdkResultCC.storeCard = true
+        XCTAssertTrue(sdkResultCC.storeCard)
+
+        sdkResultCC.storeCard = false
+        XCTAssertFalse(sdkResultCC.storeCard)
+
+        sdkResultCC.storeCard = true
+        XCTAssertTrue(sdkResultCC.storeCard)
+
+        var sdkResultExistingCC: BSExistingCcSdkResult = BSExistingCcSdkResult(sdkRequestBase: sdkRequest, shopper: BSShopper(), existingCcDetails: BSCreditCardInfo(creditCard: BSCreditCard(), billingDetails: nil))
+        XCTAssertTrue(sdkResultExistingCC.storeCard)
+
+        sdkResultExistingCC.storeCard = false
+        XCTAssertTrue(sdkResultExistingCC.storeCard)
+
+        sdkResultExistingCC.storeCard = true
+        XCTAssertTrue(sdkResultExistingCC.storeCard)
+
+        sdkResultExistingCC.storeCard = false
+        XCTAssertTrue(sdkResultExistingCC.storeCard)
     }
 }
